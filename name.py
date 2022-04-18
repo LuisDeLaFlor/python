2e4852cd47e14d433b701f8cfe7e9645cbfdf91d
LuisDeLaFlor <90716604+LuisDeLaFlor@users.noreply.github.com>
2022-04-17 20:49:25 -0500

    Create name.py

---
+++ name.py
@@ -0,0 +1,13 @@
+name = input("Ingresa tu nombre: ")
+
+last_name=input("Ingresa tu apellido: ")
+
+age=input("Ingresa tu edad: ")
+
+Nombre = f"Nombre: {name}"
+Apellido = f"Apellido: {last_name}"
+Edad = f"Edad: {age}"
+
+print(Nombre)
+print(Apellido)
+print(Edad)
